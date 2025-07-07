862e80724b173e00c6394331791f24b457e5f75f
siddharth <codehubnexus@gmail.com>
2025-07-07 19:30:00 +0530

    Completed node

--- Node/index.js
+++ Node/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplates = require("./modules/replaceTemplates");
 
 // blocking
 // Synchrous Manner
@@ -38,17 +39,43 @@ const url = require("url");
 //     console.log("File Writeten Succesfully");
 //   }
 // );
-// Creating Serverconstr server
+
+// Read Files
+const overview = fs.readFileSync("./templates/overview.html", "utf-8");
+const product = fs.readFileSync("./templates/product.html", "utf-8");
+const productData = JSON.parse(fs.readFileSync("./dev-data/data.json"));
+const card = fs.readFileSync("./templates/card.html", "utf-8");
+
+// Creating Server
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  //   const { query, pathname } = url.parse(req.url, true);
-  //   console.log(pathname);
-  if ((req.url = "/about")) {
-    res.end("we are abouty [age");
+  // console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
+  // console.log(pathname);
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardHtml = productData
+      .map((prod) => replaceTemplates(card, prod))
+      .join("");
+    // console.log(cardHtml);
+    const output = overview.replace("{%PRODUCT_CARDS%}", cardHtml);
+
+    res.end(output);
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    console.log(query);
+
+    const prod = productData[query.id];
+    let output = replaceTemplates(product, prod);
+    res.end(output);
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(JSON.stringify(productData));
+  } else {
+    res.end("Page not found please check the url");
   }
-  res.end("We have created our first Server");
 });
 
+// Starting the server
 PORT_NO = 8000;
 server.listen(PORT_NO, "127.0.0.1", () => {
   console.log("server started on port", PORT_NO);
